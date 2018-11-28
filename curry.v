3cddd2db3e975a0bf55d11574dfcdbc96aea5e06
Ignacio Tiraboschi <ignatirabo@gmail.com>
2018-11-28 14:00:05 +0100

    Saving progress this far because it's too much stuff

--- curry.v
+++ curry.v
@@ -1,4 +1,4 @@
-From Mtac2 Require Import Base Mtac2 Sorts MTele.
+From Mtac2 Require Import Base Mtac2 Sorts MTele MFixDef.
 Import Sorts.S.
 Import M.notations.
 Import M.M.
@@ -14,6 +14,7 @@ Inductive TyTree : Type :=
 | tyTree_In (s : Sort) {m : MTele} (F : InF s m) : TyTree
  | tyTree_imp (T : TyTree) (R : TyTree) : TyTree
 | tyTree_FATele {m : MTele} (T : MTele_Ty m) (F : forall t : MTele_val T, TyTree) : TyTree
+| tyTree_FATele1 {m : MTele} (T : Type) (F : forall t : T, TyTree) : TyTree
  | tyTree_FA (T : Type) (F : T -> TyTree) : TyTree
  | tyTree_FAType (F : Type -> TyTree) : TyTree
  | tyTree_base (T : Type) : TyTree
@@ -156,6 +157,59 @@ Definition checker' : forall (p : bool) (l : bool) (T : TyTree), M (checker p l 
     | _ => raise NotProperType
     end.
 
+(** Given an MTele_Ty value and an UNCURRY, it returns the type after applying the values *)
+Fixpoint RETURN {s : Sort} {m : MTele} : MTele_Sort s m -> UNCURRY m -> s :=
+  match m with
+  | mBase => fun T _ => T (* It's the base so only T : MTele_Ty m *)
+  | mTele f => fun T '(existT _ x U) => RETURN (T x) U (* U is the rest of the tuple, add a value to T in each step *)
+  end.
+
+(** It uncurries an MFA T transforming it to an "UNCURRY" *)
+Fixpoint uncurry {m : MTele} :
+  forall {T : MTele_Ty m},
+  MFA T -> forall U : UNCURRY m, M (@RETURN SType _ T U) :=
+  match m as m return
+        forall T : MTele_Ty m,
+          MTele_val (MTele_C SType SProp M T) -> forall U : UNCURRY m, M (@RETURN SType _ _ U)
+  with
+  | mBase => fun T F _ => F (* Just return F : MFA T *)
+  | mTele f => fun T F '(existT _ x U) => uncurry (F x) U (* Apply x to the MFA T and keep going *)
+  end.
+
+(** Same as above, but for MTele_val *)
+Fixpoint uncurry_val {s : Sort} {m : MTele} :
+  forall {A : MTele_Sort s m},
+  MTele_val A -> forall U : UNCURRY m, @RETURN s m A U :=
+  match m as m return
+        forall A : MTele_Sort s m,
+          MTele_val A -> forall U : UNCURRY m, @RETURN s m A U
+  with
+  | mBase => fun A F _ => F
+  | mTele f => fun A F '(existT _ x U) => @uncurry_val s (f x) _ (App F x) _
+  end.
+
+Definition uncurry_in {s : Sort} :
+  forall {m : MTele} (F : (forall now_ty : (forall s0 : Sort, MTele_Sort s0 m -> s0),
+ (forall (s0 : Sort) (T : MTele_Sort s0 m), MTele_val T -> now_ty s0 T) -> s)),
+  (MTele_val (MTele_In s F)) ->
+  forall U : UNCURRY m, 
+    let now_ty := fun (s' : Sort) (ms : MTele_Sort s' m) => RETURN ms U in
+    let now_val := fun (s' : Sort) (ms : MTele_Sort s' m) (mv : MTele_val ms) => uncurry_val mv U in
+    F now_ty now_val.
+  fix IH 1; destruct m; intros.
+  + simpl in *. assumption.
+  + simpl in *. destruct U. specialize (IH (F x) _ (App X0 x) u). assumption.
+Defined.
+
+(** It uncurries an "UNCURRY" transforming it to an MFA T *)
+Fixpoint curry_val {s : Sort} {m : MTele} :
+  forall {A : MTele_Sort s m},
+  (forall U : UNCURRY m, @RETURN s m A U) -> MTele_val A :=
+  match m with
+  | mBase => fun A F => F tt
+  | @mTele T f => fun A F => Fun (fun a : T => curry_val (fun U => F (existT _ a U)))
+  end.
+
 Definition ShitHappens : Exception. exact exception. Qed.
 
 Let T := (tyTree_FAType (fun T : Type => tyTree_imp (tyTree_base T) (tyTree_M T))).
@@ -164,19 +218,39 @@ Let t : to_ty T := @ret.
 Fixpoint lift (m : MTele) (p l : bool) (T : TyTree) : forall (f : to_ty T) (c : checker p l T), M { T : TyTree & to_ty T} :=
   match T as T return forall (f : to_ty T) (c : checker p l T), M { T' : TyTree & to_ty T'} with
   (* | [? X] tyTree_M X => _ *)
-  (* | [? X Y] tyTree_imp X Y => _ *)
-  (* | [? X F] tyTree_FA X F => _ *)
-  | tyTree_FAType F =>
-    fun f c => 
-      c' <- checker' p l (F (MTele_Ty m)); 
-      s <- lift m p l (F (MTele_Ty m)) (f (MTele_Ty m)) c';
-      (* let T := tyTree_FATele _ _ in *)
-      ret (existT to_ty (projT1 s) (projT2 s))
-  (* | [? X] tyTree_base X =>  *)
+  (* | tyTree_imp X Y => *)
+  (*   fun f c => *)
+  (*     c_x <- checker' (negb p) true (X); *)
+  (*     X <- lift m (negb p) true X _ c_x; *)
+  (*     _ *)
+  (* | tyTree_FA X F => (* Here I should use FATele with curry/uncurry *) *)
+  (*   fun f c => *)
+  (*    \nu x : X, *)
+  (*      c' <- checker' p l (F x); *)
+  (*      s <- lift m p l (F x) (f x) c'; *)
+  (*      (* p1 <- abs_fun x (Projt1 s); *) *)
+  (*      (* p2 <- abs_fun x (projT2 s); *) *)
+  (*      \nu tty : MTele_Ty m, *)
+  (*      let p1 := tyTree_FA tty (fun v : MTele_val tty => projT1 s) in *)
+  (*      let p2 := (fun v : () => projT2 s) in *)
+  (*      ret (existT to_ty p1 p2) *)
+  | tyTree_FAType F => (* this one is the equivalent to tyTree_FATele, I think *)
+    fun f c =>
+      c' <- checker' p l (F (MTele_Ty m));
+      s <- lift m p l (F (MTele_Ty m)) (f (MTele_Ty m)) c'; (* sigT of continuation *)
+      let p1 := tyTree_FATele1 (MTele_Ty m) (projT1 s) in
+      let p2 := (projT2 s) in
+      let f := fun X : Type => projT2 s in
+      ret (existT to_ty (T) (projT2 s))
+  (* | [? X] tyTree_base X => *)
   (*   let T := tyTree_base (MTele_Ty m) in *)
   (*   let f : to_ty T = t *)
   (*   existT T *)
   | _ => fun t c => raise ShitHappens
   end.
+
+Check (@mTele nat (fun x : nat => mBase)).
+
+Eval compute in lift (mTele (fun x : nat => mBase)) true false (tyTree_FA )
 
 projT1
